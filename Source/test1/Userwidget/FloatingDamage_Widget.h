a5df74c9ce344221fa7de6fe43948d3def836d90
Cho youngun <asd09080@gmail.com>
2023-04-03 17:48:41 +0900

    데미지를 출력하는 UI

---
+++ Source/test1/Userwidget/FloatingDamage_Widget.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include <Components/Image.h>
+#include "FloatingDamage_Widget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST1_API UFloatingDamage_Widget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeTick(const FGeometry& Geometry, float DeltaSeconds) override;
+	virtual void NativeConstruct() override;
+	void SetDamage(float TakeDamege, bool cri);
+	void DeleteDamage();
+
+private:
+
+	FText damage;
+	float lifeTime;
+
+	UPROPERTY()
+	class UTextBlock* floatingDamage;
+
+	UPROPERTY()
+		class UImage* Image_Cri;
+};
