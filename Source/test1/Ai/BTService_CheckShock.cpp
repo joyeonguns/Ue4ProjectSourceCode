f278b8586f52b53eb0df4fc8ecf2688beea67a22
Cho youngun <asd09080@gmail.com>
2023-04-03 17:41:55 +0900

    주기적으로 캐릭터가 충격을 받은 상태인지 확인

---
+++ Source/test1/Ai/BTService_CheckShock.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_CheckShock.h"
+#include "Enemy_AIController.h"
+#include "EnemyBasic_Character.h"
+#include "TPS_Character.h"
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+UBTService_CheckShock::UBTService_CheckShock()
+{
+	NodeName = TEXT("CheckShock");
+	Interval = 0.5f;
+}
+
+void UBTService_CheckShock::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaTime)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaTime);
+
+	bool bResult;
+
+	APawn* ControllingPawn = OwnerComp.GetAIOwner()->GetPawn();
+
+	if (!ControllingPawn)
+		return;
+
+	auto Target = Cast<ATPS_Character>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(AEnemy_AIController::TargetKey));
+	auto Enemy = Cast<AEnemyBasic_Character>(OwnerComp.GetAIOwner()->GetPawn());
+
+
+
+	bResult = (Enemy->GetCheckShocking());
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsBool(AEnemy_AIController::bShockKey, bResult);
+
+	if (bResult) {
+		auto pl = Cast<ATPS_Character>(UGameplayStatics::GetPlayerCharacter(GetWorld(),0));
+		OwnerComp.GetBlackboardComponent()->SetValueAsObject(AEnemy_AIController::TargetKey, pl);
+	}
+}
